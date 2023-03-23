860a0cf5f64607c5141e06944616da706bbd4266
NealeAust <nealechapman@optushome.com.au>
2023-03-23 13:00:44 +1100

    loading to Heroku

--- config/connection.js
+++ config/connection.js
@@ -2,26 +2,16 @@
 // Import the Sequelize constructor library
 
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-// Create connection to tech_blog_db database - shema.sql
-// Pass in mysql your information for username & password.
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-  } else {
-    sequelize = new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-      }
-    );
-  }
-  
-  module.exports = sequelize;
+module.exports = sequelize;
